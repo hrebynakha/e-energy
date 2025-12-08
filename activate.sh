da07feca7bdc9c3c756e0cef6066e13f88b5206f
Hrebynakha Anatolii <hrebynakha@gmail.com>
2025-12-08 12:36:10 +0200

    add install scripts

---
+++ activate.sh
@@ -0,0 +1,3 @@
+sudo apt install python3.13-venv
+python3 -m venv venv
+source venv/bin/activate
--- install.sh
+++ install.sh
@@ -1,6 +1,32 @@
-sudo apt install python3.13-venv
-python3 -m venv venv
-source venv/bin/activate
-
-pip install -r requirements.txt 
 sudo apt install -y build-essential python3-dev libxml2-dev libxslt1-dev
+pip install -r requirements.txt 
+python main.py --run makemigrations
+python main.py --run migrate
+
+cp .env.example .env
+
+
+sudo apt install nginx
+
+cp conf/gunicorn.socket /etc/systemd/system/gunicorn.socket
+cp conf/gunicorn.service /etc/systemd/system/gunicorn.service
+cp conf/energybot.service /etc/systemd/system/energybot.service
+
+sudo systemctl daemon-reload
+
+cp conf/nginx.conf /etc/nginx/conf.d/e-energy.conf 
+
+IP=$(hostname -I | awk '{print $1}')
+sudo sed -i "s/server_domain_or_IP/$IP/g" /etc/nginx/conf.d/e-energy.conf
+
+
+sudo systemctl start gunicorn.socket
+sudo systemctl enable gunicorn.socket
+
+
+
+sudo nginx -t
+sudo systemctl restart nginx
+
+echo "Installation completed successfully!"
+echo "Got to http://$IP to see the result"
